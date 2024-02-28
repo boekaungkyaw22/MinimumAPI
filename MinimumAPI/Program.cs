ecd2d52419b072557bcac7598e5e752aa1683997
Boe_Kaung_Kyaw <boekaung.kyaw@kbzbank.com>
2024-02-29 00:10:38 +0630

    Minimum API

--- MinimumAPI/Program.cs
+++ MinimumAPI/Program.cs
@@ -8,23 +8,14 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Routing
+// "/shirts"
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/shirts", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return "Reading all the shirts";
 });
+
 
 app.Run();
 

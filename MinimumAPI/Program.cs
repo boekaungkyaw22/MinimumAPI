2f600786e2e6ea34b9539a889fdf19539122faf4
Boe_Kaung_Kyaw <boekaung.kyaw@kbzbank.com>
2024-03-01 00:38:11 +0630

    'update"

--- MinimumAPI/Program.cs
+++ MinimumAPI/Program.cs
@@ -21,6 +21,21 @@ app.MapGet("/shirts/{id}", (int id) =>
     return $"Reading the shift Id: {id}";
 });
 
+app.MapPost("/shirts", () =>
+{
+    return "Creating a shirts";
+});
+
+app.MapPut("/shirts/{id}", (int id) =>
+{
+    return $"Updating shirt with ID: {id}";
+});
+
+app.MapDelete("/shirts/{id}", (int id) =>
+{
+    return $"Deleting a shirt with ID: {id}";
+});
+
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
